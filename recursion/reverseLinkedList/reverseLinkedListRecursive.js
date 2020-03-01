ac4e82854a9aedb91c70f27fee77f3497c54de65
Kenneth Yu <kenneth.a.yu@gmail.com>
2020-03-01 12:11:17 -0800

    make the reverseList solution return the new head

--- recursion/reverseLinkedList/reverseLinkedListRecursive.js
+++ recursion/reverseLinkedList/reverseLinkedListRecursive.js
@@ -25,6 +25,7 @@ function ListNode(val) {
 }
 
 let reverseList = (head) => {
+  let newHead
   // 1->2->3->4->5->null
   // 5->4->3->2->1->NULL
   //             1->NULL
@@ -42,7 +43,8 @@ let reverseList = (head) => {
     if (currentNode === null) {
       // :: SOLVING FOR BASECASE ::
       // currentNode == 5
-      console.log("BASE CASE REACHED")
+      // console.log("BASE CASE REACHED")
+      newHead = currentNode
       return
     }
     if (index === 0) {
@@ -50,7 +52,7 @@ let reverseList = (head) => {
       currentNode // 1
       originalNext = currentNode.next // 2
       currentNode.next = null // 1->null
-      console.log(`${currentNode.val}->null`)
+      // console.log(`${currentNode.val}->null`)
     }
     else {
       // :: 2nd ITERATION ::
@@ -59,30 +61,31 @@ let reverseList = (head) => {
       currentNode // 2
       originalNext = currentNode.next // 3
       currentNode.next = lastUsedNode // 2->1
-      console.log(`${currentNode.val}->${currentNode.next.val}`)
+      // console.log(`${currentNode.val}->${currentNode.next.val}`)
     }
     helper(originalNext, currentNode, index+1)
   }
   helper(head)
+  return newHead
 }
 
-let l1 = new ListNode(1)
-let l2 = new ListNode(2)
-let l3 = new ListNode(3)
-let l4 = new ListNode(4)
-let l5 = new ListNode(5)
+// let l1 = new ListNode(1)
+// let l2 = new ListNode(2)
+// let l3 = new ListNode(3)
+// let l4 = new ListNode(4)
+// let l5 = new ListNode(5)
 
-l1.next = l2
-l2.next = l3
-l3.next = l4
-l4.next = l5
+// l1.next = l2
+// l2.next = l3
+// l3.next = l4
+// l4.next = l5
 
-console.log(buildList(l1))
+// console.log(buildList(l1))
 
 // :: DESIRED OUTCOME ::
 // INITIAL: 1->2->3->4->5->NULL
 // RESULT : 5->4->3->2->1->NULL
 
-reverseList(l1)
-console.log(buildList(l5))
-console.log(buildList(l1.next) == null)
+// reverseList(l1)
+// console.log(buildList(l5))
+// console.log(buildList(l1.next) == null)
