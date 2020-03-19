d123f77961602dd3d8da1f634ca0c227cda0a98a
Kenneth Yu <kenneth.a.yu@gmail.com>
2020-03-18 22:43:09 -0700

    update count_and_say_solution

--- interviewQuestions/count_and_say/count_and_say.rb
+++ interviewQuestions/count_and_say/count_and_say.rb
@@ -7,10 +7,13 @@
   {FREQUENCY}{VALUE}
 SAMPLE
 
+# TIME COMPLEXITY: O(N)
+# SPACE COMPLEXITY: O(N) where N is equal to the length of the string
+# NOTE: Could have done memoization...
 
 def decode(number)
-  if number == 1
-    return 11
+  if number == "1"
+    return "11"
   end
   stringified_number = number.to_s
   consecutive_count = 1
@@ -19,40 +22,36 @@ def decode(number)
   i = 1
   while (i < stringified_number.length) do
     if last_evaluated_number == stringified_number[i]
-      # puts "DECODING --> MATCH FOUND... #{last_evaluated_number} == #{stringified_number[i]}"
       consecutive_count += 1
     else
-      # puts "DECODING --> MISMATCH... #{last_evaluated_number} != #{stringified_number[i]}"
       result += "#{consecutive_count}#{last_evaluated_number}"
       consecutive_count = 1
       last_evaluated_number = stringified_number[i]
     end
-    if i == stringified_number.length-1 #
-      # puts "DECODING --> FINAL STEP... Concatenating #{consecutive_count}#{last_evaluated_number}"
+    if i == stringified_number.length-1
       result += "#{consecutive_count}#{last_evaluated_number}"
     end
     i += 1
   end
-  # puts "FINAL RESULT '#{result}'"
-  return result.to_i
+  return result
 end
 
 def count_and_say(n)
   if (n == 1)
-    return 1
+    return "1"
   end
   decode(count_and_say(n-1))
 end
 
 
-p count_and_say(1) == 1
-p count_and_say(2) == 11
-p count_and_say(3) == 21
-p count_and_say(4) == 1211
-p count_and_say(5) == 111221
-p count_and_say(6) == 312211
-p count_and_say(7) == 13112221
-p count_and_say(8) == 1113213211
-p count_and_say(9) == 31131211131221
-p count_and_say(10) == 13211311123113112211
+p count_and_say(1) == "1"
+p count_and_say(2) == "11"
+p count_and_say(3) == "21"
+p count_and_say(4) == "1211"
+p count_and_say(5) == "111221"
+p count_and_say(6) == "312211"
+p count_and_say(7) == "13112221"
+p count_and_say(8) == "1113213211"
+p count_and_say(9) == "31131211131221"
+p count_and_say(10) == "13211311123113112211"
 
