0446bfd5dc16b3282b121c9fa9969439dbb7ed79
Kenneth Yu <kenneth.a.yu@gmail.com>
2020-04-28 15:43:50 -0700

    add solution for work_break_dynamic_programming

---
+++ leetcode/word_break/solution_word_break_dynamic_programming.js
@@ -0,0 +1,14 @@
+var wordBreak = function (s, wordDict) {
+  let dp = new Array(s.length + 1).fill(false);
+  dp[0] = true;
+  for (let i = 1; i <= s.length; i++) {
+    for (let j = 0; j < i; j++) {
+      const subStr = s.substring(j, i);
+      if (dp[j] && wordDict.includes(subStr)) {
+        dp[i] = true;
+        break;
+      }
+    }
+  }
+  return dp[s.length];
+};
