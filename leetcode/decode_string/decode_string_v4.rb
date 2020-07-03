2cce35df8bbf61c87f8d10afd3d269722c1c203b
Kenneth Yu <kenneth.a.yu@gmail.com>
2020-07-03 14:11:16 -0700

    complete decode_string_v4

--- leetcode/decode_string/decode_string_v4.rb
+++ leetcode/decode_string/decode_string_v4.rb
@@ -8,8 +8,19 @@ def is_fully_computed?(s,open_idx,close_idx)
   return s.length - 1 == close_idx && open_idx == 1
 end
 
+def get_coefficient(s,open_idx,set_of_stringified_numbers)
+  coefficient_str = ""
+  idx_to_check = open_idx -1
+  while (idx_to_check >= 0 && set_of_stringified_numbers.include?(s[idx_to_check])) do
+    coefficient_str.prepend(s[idx_to_check])
+    idx_to_check -= 1
+  end
+  return coefficient_str.to_i
+end
+
 def decode_string(s)
   stack_of_open_bracket_indexes = []
+  set_of_stringified_numbers = Set.new("0".."9")
   i = 0
   until s[i] == nil do
     char = s[i]
@@ -17,16 +28,16 @@ def decode_string(s)
     if char == "]"
       open_idx = stack_of_open_bracket_indexes.pop
       close_idx = i
-      coefficient = s[open_idx - 1].to_i
+      coefficient = get_coefficient(s,open_idx,set_of_stringified_numbers)
       mid = compute(s,coefficient,open_idx,close_idx)
-      if open_idx > 1
-        front = s[0..open_idx-2]
+      if open_idx - coefficient.to_s.length - 1 > 0
+        front = s[0..open_idx - coefficient.to_s.length - 1]
       else
         front = ""
       end
       back = s[close_idx+1..s.length-1]
       s = front.concat(mid).concat(back)
-      i = open_idx - 2 + mid.length # set the idx to be at the edge of the newly created mid. subtract by 1 since i gets incremented after
+      i = open_idx - coefficient.to_s.length + mid.length - 1 # set the idx to be at the edge of the newly created mid. subtract by 1 since i gets incremented after
     end
     i += 1
   end
@@ -39,6 +50,15 @@ module TestSuite
     s = "3[a]2[bc]"
     p decode_string(s) == "aaabcbc"
   end
+
+  <<-COMMENTS
+    "3[a]2[bc]"
+    "3[a]bcbc"
+    front = "3[a]"
+    mid = "bcbc"
+    back = ""
+    s = "3[a]bcbc"
+  COMMENTS
 
   def self.t2
     s = "2[abc]3[cd]ef"
@@ -54,12 +74,25 @@ module TestSuite
     s = "3[a2[c]]"
     p decode_string(s) == "accaccacc"
   end
+  <<-COMMENTS
+    "3[a2[c]]"
+    front = "3[a"
+    mid = "cc"
+    back =  "]"
+    s = 3[acc]
+  COMMENTS
+
+  def self.t5
+    s = "10[bc]"
+    p decode_string(s) == "bcbcbcbcbcbcbcbcbcbc"
+  end
 
   def self.run_tests
     t1
     t2
     t3
     t4
+    t5
   end
 
 end
