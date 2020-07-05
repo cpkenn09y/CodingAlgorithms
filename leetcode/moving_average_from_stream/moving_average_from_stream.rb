841aa7352a1fd1eb26c24e43ce3c21cc34c6e55b
Kenneth Yu <kenneth.a.yu@gmail.com>
2020-07-04 17:34:18 -0700

    complete moving_average_from_stream challenge
    NOTE: also added vscode configurations to gitignore folder

---
+++ leetcode/moving_average_from_stream/moving_average_from_stream.rb
@@ -0,0 +1,30 @@
+class MovingAverage
+
+  def initialize(size)
+    @max_size = size
+    @collection = []
+    @sum = 0
+  end
+
+  def next(val)
+    if @collection.length >= @max_size
+      old_num = @collection.shift
+      @sum -= old_num
+    end
+    @collection.push(val)
+    @sum = (@sum + val).to_f
+    @current_average = @sum / @collection.length # TODO: Handle when we are at max size
+    return @current_average
+  end
+
+end
+
+# Your MovingAverage object will be instantiated and called as such:
+# obj = MovingAverage.new(size)
+# param_1 = obj.next(val)
+
+m = MovingAverage.new(3);
+p m.next(1) == 1
+p m.next(10) == (1 + 10).to_f / 2
+p m.next(3) == (1 + 10 + 3).to_f / 3
+p m.next(5) == (10 + 3 + 5).to_f / 3
